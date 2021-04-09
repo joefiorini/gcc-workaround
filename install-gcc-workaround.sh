cf107e499c9c195ad5450f107b04737b8bfda209
Joe Fiorini <joe@joefiorini.com>
2021-04-09 12:47:30 -0400

    Update install-gcc-workaround.sh

--- install-gcc-workaround.sh
+++ install-gcc-workaround.sh
@@ -11,9 +11,7 @@ if [ "$(uname)" == 'Darwin' ]; then
     MAJOR_VERSION_TO_PATCH="10"
     MAJOR_VERSION=$($CC -dumpversion | awk -F. '{ print $1 }' )
 
-    if [ $MAJOR_VERSION -ge $MAJOR_VERSION_TO_PATCH ]; then
         echo "#! $SH" >> $cur__bin/gcc
         echo "exec $CC -fcommon '\"\$@\"'" >> $cur__bin/gcc
         chmod +x $cur__bin/gcc
-    fi
 fi
